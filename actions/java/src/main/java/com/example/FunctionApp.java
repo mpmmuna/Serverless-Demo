e60202c3426cb46b73ed55662fe4282a93ea35c7
mpmmuna <39486998+mpmmuna@users.noreply.github.com>
2018-08-09 08:23:38 +0530

    Update FunctionApp.java

--- actions/java/src/main/java/com/example/FunctionApp.java
+++ actions/java/src/main/java/com/example/FunctionApp.java
@@ -25,7 +25,7 @@ import com.google.gson.JsonObject;
 public class FunctionApp {
   public static JsonObject main(JsonObject args) {
     JsonObject response = new JsonObject();
-    response.addProperty("message", "Hello! Welcome to WSO2Con Asia 2018 Started");
+    response.addProperty("message", "Hello! Welcome to WSO2Con Asia 2018");
     return response;
   }
 }
